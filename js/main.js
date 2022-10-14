4b14ab095b3b27bad84496589db6579dc0d2b658
MaksimKasyanov <83403673+MaksimKasyanov@users.noreply.github.com>
2022-10-14 08:33:50 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.expander__card');
+cards.forEach((el) => el.addEventListener('mouseover', expandCard));
+cards.forEach((el) => el.addEventListener('mouseout', defaultCard));
+function expandCard() {
+	cards.forEach((el) => el.classList.remove('expander__card-active'));
+	this.classList.add('expander__card-active');
+}
+function defaultCard(){
+	cards.forEach((el) => el.classList.remove('expander__card-active'));
+	cards[0].classList.add('expander__card-active');
+}

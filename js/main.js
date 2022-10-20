e66e88eb13c5d7820f3eb97e6a2483a7c3a22a45
MaksimKasyanov <83403673+MaksimKasyanov@users.noreply.github.com>
2022-10-20 09:42:29 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-const cards = document.querySelectorAll('.expander__card');
-cards.forEach((el) => el.addEventListener('mouseenter', expandCard));
-cards.forEach((el) => el.addEventListener('mouseleave', defaultCard));
+const blocks = document.querySelectorAll('.block');
 
-function expandCard() {
-	cards.forEach((el) => el.classList.remove('expander__card-active'));
-	this.classList.add('expander__card-active');
-}
+window.addEventListener('scroll', checkBlocks);
+function checkBlocks() {
+	const scrollTrigger = window.innerHeight / 5 * 4;
 
-function defaultCard(){
-	cards.forEach((el) => el.classList.remove('expander__card-active'));
-	cards[0].classList.add('expander__card-active');
+	blocks.forEach(block => {
+		const blockTop = block.getBoundingClientRect().top;
+		if(blockTop < scrollTrigger){
+			block.classList.add('show')
+		}
+		if(blockTop > scrollTrigger){
+			block.classList.remove('show')
+		}
+	})
+
 }

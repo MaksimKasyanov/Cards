cd9e344989b33c328adc5286d7c74ea74bc4b12a
MaksimKasyanov <83403673+MaksimKasyanov@users.noreply.github.com>
2022-11-05 19:26:19 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 // !Expanding cards
 
 if (window.innerWidth > 1000) {
-	const cards = document.querySelectorAll('.expander__card');
+	const cards = document.querySelectorAll('.expander__immigration .expander__card');
+	cards.forEach((el) => el.addEventListener('mouseenter', expandCard));
+	cards.forEach((el) => el.addEventListener('mouseleave', defaultCard));
+
+	function expandCard() {
+		cards.forEach((el) => el.classList.remove('expander__card-active'));
+		this.classList.add('expander__card-active');
+	}
+
+	function defaultCard() {
+		cards.forEach((el) => el.classList.remove('expander__card-active'));
+		cards[0].classList.add('expander__card-active');
+	}
+}
+if (window.innerWidth > 1000) {
+	const cards = document.querySelectorAll('.expander__relocation .expander__card');
 	cards.forEach((el) => el.addEventListener('mouseenter', expandCard));
 	cards.forEach((el) => el.addEventListener('mouseleave', defaultCard));
 
@@ -17,91 +32,61 @@ if (window.innerWidth > 1000) {
 }
 
 // ! Scroll ainmated paragraphs
-const accordionParagraphs = document.querySelectorAll('.accordion__paragraph');
+const relocationParagraphs = document.querySelectorAll('.accordion__relocation .accordion__paragraph');
+const differencesParagraphs = document.querySelectorAll('.accordion__differences .accordion__paragraph');
 const accordionImages = document.querySelectorAll('.accordion__img');
-const accordionWrapper = document.querySelector('.accordion__wrapper');
+const accordionRelocation = document.querySelector('.accordion__relocation .accordion__paragraphs-wrapper');
+const accordionDifferences = document.querySelector('.accordion__differences .accordion__paragraphs-wrapper');
 window.addEventListener('scroll', checkParagraphs);
 
 function checkParagraphs() {
 	const scrollTrigger = window.innerHeight;
-	accordionParagraphs.forEach((el) => {
-		const blockTop = accordionWrapper.getBoundingClientRect().top;
+	console.log(relocationParagraphs, differencesParagraphs)
+	relocationParagraphs.forEach((el) => {
+		const blockTop = accordionRelocation.getBoundingClientRect().top;
 		if (blockTop < (scrollTrigger / 10) * 8) {
-			accordionParagraphs.forEach((el) =>
+			relocationParagraphs.forEach((el) =>
 				el.classList.remove('accordion__paragraph-active')
 			);
-			accordionParagraphs[0].classList.add('accordion__paragraph-active');
+			relocationParagraphs[0].classList.add('accordion__paragraph-active');
 		}
 		if (blockTop < (scrollTrigger / 10) * 6) {
-			accordionParagraphs.forEach((el) =>
+			relocationParagraphs.forEach((el) =>
 				el.classList.remove('accordion__paragraph-active')
 			);
-			accordionParagraphs[1].classList.add('accordion__paragraph-active');
+			relocationParagraphs[1].classList.add('accordion__paragraph-active');
 		}
 		if (blockTop < (scrollTrigger / 10) * 4) {
-			accordionParagraphs.forEach((el) =>
+			relocationParagraphs.forEach((el) =>
 				el.classList.remove('accordion__paragraph-active')
 			);
-			accordionParagraphs[2].classList.add('accordion__paragraph-active');
+			relocationParagraphs[2].classList.add('accordion__paragraph-active');
 		}
 	});
-	// accordionImages.forEach((el) => {
-	// 	const blockTop = accordionWrapper.getBoundingClientRect().top;
-	// 	if (blockTop < (scrollTrigger / 10) * 8) {
-	// 		accordionImages.forEach((el) =>
-	// 			el.classList.remove('accordion__img-active')
-	// 		);
-	// 		accordionImages[0].classList.add('accordion__img-active');
-	// 	}
-	// 	if (blockTop < (scrollTrigger / 10) * 6) {
-	// 		accordionImages.forEach((el) =>
-	// 			el.classList.remove('accordion__img-active')
-	// 		);
-	// 		accordionImages[1].classList.add('accordion__img-active');
-	// 	}
-	// 	if (blockTop < (scrollTrigger / 10) * 4) {
-	// 		accordionImages.forEach((el) =>
-	// 			el.classList.remove('accordion__img-active')
-	// 		);
-	// 		accordionImages[2].classList.add('accordion__img-active');
-	// 	}
-	// });
+	differencesParagraphs.forEach((el) => {
+		const blockTop = accordionDifferences.getBoundingClientRect().top;
+		if (blockTop < (scrollTrigger / 10) * 8) {
+			differencesParagraphs.forEach((el) =>
+				el.classList.remove('accordion__paragraph-active')
+			);
+			differencesParagraphs[0].classList.add('accordion__paragraph-active');
+		}
+		if (blockTop < (scrollTrigger / 10) * 6) {
+			differencesParagraphs.forEach((el) =>
+				el.classList.remove('accordion__paragraph-active')
+			);
+			differencesParagraphs[1].classList.add('accordion__paragraph-active');
+		}
+		if (blockTop < (scrollTrigger / 10) * 4) {
+			differencesParagraphs.forEach((el) =>
+				el.classList.remove('accordion__paragraph-active')
+			);
+			differencesParagraphs[2].classList.add('accordion__paragraph-active');
+		}
+	});
 }
 
-// ! Slider
-
-// let mySlider = new Swiper(".carousel", {
-// 	effect: "coverflow",
-// 	grabCursor: true,
-// 	centeredSlides: true,
-// 	slidesPerView: "auto",
-// 	spaceBetween: 20,
-// 	speed: 800,
-// 	loop: true,
-
-// 	navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	 },
-
-// 	autoplay: {
-// 		delay: 2000,
-// 		disableOnInteraction: false,
-// 		pauseOnMouseEnter:true,
-// 	},
-
-// 	coverflowEffect: {
-// 		rotate: 0,
-// 		stretch: 0,
-// 		depth: 0,
-// 		modifier: 1,
-// 		slideShadows: false,
-// 	},
-
-// 	pagination: {
-// 		el: ".swiper-pagination",
-// 	},
-// });
+// ! Slider carousel
 
 let carousel = new Swiper('.carousel', {
 	wrapperClass: 'carousel__wrapper',
@@ -139,3 +124,33 @@ let carousel = new Swiper('.carousel', {
 		bulletActiveClass: 'carousel__bullet-active',
 	},
 });
+
+// ! Slider cards
+let cardsLeft = new Swiper(".cards__left", {
+	wrapperClass: 'cards__left-wrapper',
+	slideClass: 'cards__left-slide',
+	slideActiveClass: 'cards__left-active',
+	effect: "fade",
+	grabCursor: false,
+	loop: true,
+	navigation: {
+		nextEl: ".cards__next",
+		prevEl: ".cards__prev",
+	},
+});
+
+let cardsRight = new Swiper(".cards__right", {
+	wrapperClass: 'cards__right-wrapper',
+	slideClass: 'cards__right-slide',
+	effect: "cards",
+	grabCursor: true,
+	loop: true,
+	cardsEffect: {
+		slideShadows: false,
+		perSlideRotate: 4,
+		perSlideOffset: 3,
+	},
+});
+
+cardsLeft.controller.control = cardsRight;
+cardsRight.controller.control = cardsLeft;
